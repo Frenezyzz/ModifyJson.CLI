8aece0d43dc8c77f8e9d30b8518b8083c2b9163c
Aron Elias Ibañez <70858294+Frenezyzz@users.noreply.github.com>
2022-04-20 23:25:26 -0300

    Delete fileJson.py

--- fileJson.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-import json
-class FileJson(ABC):
-
-    @abstractmethod
-    def __init__(self):       
-        self.data = json.loads(open("data.json","r").read())
